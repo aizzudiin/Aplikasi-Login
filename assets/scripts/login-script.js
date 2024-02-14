b25c8d13abadf1b6ca1fb1673015d8afa54defff
aizzudiin <160003817+aizzudiin@users.noreply.github.com>
2024-02-14 17:17:19 +0700

    Update login-script.js
    Mengganti Comment sesuai dengan format JsDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,24 +1,43 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/**
+* Deklarasi variabel untuk setiap elemen View
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+* Deklarasi Variabel yang menyimpan alamat Email & Password
+* @constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* Comment : Bagian dari Button Submit */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+  * Deklarasi variabel email dan Password untuk mendapatkan nilai input pengguna
+  * @constant {string}
+  */
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+  /* Comment : percabangan untuk memastikan apakah Nilai email sudah sesuai dengan yang tersimpan */
   if (email == expectedEmail && password == expectedPassword) {
+    /* Comment : jika sesuai maka akan pergi ke halaman Home */
     goToHome();
+  /* Comment : selain itu jika tidak sesuai maka akan diberikan popup error */ 
   } else {
     showPopUp();
   }
